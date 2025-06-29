ce0bec1bf0ad2d77fc045fa3770cfb8d993846f8
Eduardo <Em.87.webs@gmail.com>
2025-06-29 15:45:28 +0200

    Refactor assembly to single-instruction lines

--- src/main.asm
+++ src/main.asm
@@ -46,24 +46,48 @@ MainLoop:
 .read_input_loop:
     call ReadJoypadWithDebounce
     ld a, [JoyState]
-    bit BUTTON_UP_BIT, a,   jr nz, .move_up
-    bit BUTTON_DOWN_BIT, a, jr nz, .move_down
-    bit BUTTON_A_BIT, a,    jr nz, .select_item
+    bit BUTTON_UP_BIT, a
+    jr nz, .move_up
+    bit BUTTON_DOWN_BIT, a
+    jr nz, .move_down
+    bit BUTTON_A_BIT, a
+    jr nz, .select_item
     jr .read_input_loop
 
 .move_up:
-    ld a, [CursorIndex], or a, jr z, .read_input_loop
-    dec a, ld [CursorIndex], a, call PlayBeepNav, jp MainLoop
+    ld a, [CursorIndex]
+    or a
+    jr z, .read_input_loop
+    dec a
+    ld [CursorIndex], a
+    call PlayBeepNav
+    jp MainLoop
 .move_down:
-    ld a, [CursorIndex], cp MENU_ITEMS - 1, jr z, .read_input_loop
-    inc a, ld [CursorIndex], a, call PlayBeepNav, jp MainLoop
+    ld a, [CursorIndex]
+    cp MENU_ITEMS - 1
+    jr z, .read_input_loop
+    inc a
+    ld [CursorIndex], a
+    call PlayBeepNav
+    jp MainLoop
 
 .select_item:
     call PlayBeepConfirm
-    ld a, ENTRY_NEW, ld [EntryReason], a
-    ld a, 1, call SwitchBank
-    ld a, [CursorIndex], ld c, a, ld b, 0, ld hl, EntryPoints, add hl, bc, add hl, bc
-    ld e, [hl], inc hl, ld d, [hl], push de, ret
+    ld a, ENTRY_NEW
+    ld [EntryReason], a
+    ld a, 1
+    call SwitchBank
+    ld a, [CursorIndex]
+    ld c, a
+    ld b, 0
+    ld hl, EntryPoints
+    add hl, bc
+    add hl, bc
+    ld e, [hl]
+    inc hl
+    ld d, [hl]
+    push de
+    ret
 
 ; --- Rutinas de Inicialización ---
 InitGlobalVars:
@@ -82,65 +106,150 @@ InitSubsystems:
     ret
 
 InitInterrupts:
-    di, xor a, ld [rIF], a, ld a, (1 << IEF_VBLANK), ld [rIE], a, ei, ret
+    di
+    xor a
+    ld [rIF], a
+    ld a, (1 << IEF_VBLANK)
+    ld [rIE], a
+    ei
+    ret
 
 InitVRAM:
     push af, bc, hl
     call WaitVBlank
-    xor a, ld [rLCDC], a
-    ld hl, $8000, ld bc, $1800, call FillMemory
+    xor a
+    ld [rLCDC], a
+    ld hl, $8000
+    ld bc, $1800
+    call FillMemory
     call LoadFont
-    ld a, %11100100, ld [rBGP], a
-    ld a, LCDCF_ON | LCDCF_BG8000 | LCDCF_BG9800 | LCDCF_BGON, ld [rLCDC], a
-    pop hl, bc, af, ret
+    ld a, %11100100
+    ld [rBGP], a
+    ld a, LCDCF_ON | LCDCF_BG8000 | LCDCF_BG9800 | LCDCF_BGON
+    ld [rLCDC], a
+    pop hl, bc, af
+    ret
 
 LoadFont:
     push af, bc, de, hl
-    ld hl, FontData, ld de, $8000 + (" " * 16), ld bc, 96 * 16, call CopyString
-    pop hl, de, bc, af, ret
+    ld hl, FontData
+    ld de, $8000 + (" " * 16)
+    ld bc, 96 * 16
+    call CopyString
+    pop hl, de, bc, af
+    ret
 
 ; --- Flujo y UI ---
 VBlankHandler:
     push af, bc, de, hl
-    ld a, [FrameCounter], inc a, ld [FrameCounter], a
+    ld a, [FrameCounter]
+    inc a
+    ld [FrameCounter], a
     ; Sound_Update podría ir aquí
-    pop hl, de, bc, af, reti
+    pop hl, de, bc, af
+    reti
 
 SwitchBank:
-    ld [CurrentBank], a, ld [$2000], a, ret
+    ld [CurrentBank], a
+    ld [$2000], a
+    ret
 
 ExitGame:
-    call SRAM_Init, xor a, ld [rNR52], a, jp $0000
+    call SRAM_Init
+    xor a
+    ld [rNR52], a
+    jp $0000
 
 ShowWarning:
     push af, bc, de, hl
-    call UI_ClearScreen, ld a, 2, ld b, 4, ld c, 16, ld d, 10, call UI_DrawBox
-    ld hl, WarningTitle, ld c, 2, ld d, 4, ld e, 16, call UI_PrintInBox
-    ld hl, WarningMsg1, ld d, 7, ld e, 4, call UI_PrintStringAtXY
-    ld hl, WarningMsg2, ld d, 8, ld e, 4, call UI_PrintStringAtXY
-    ld hl, WarningMsg3, ld d, 9, ld e, 4, call UI_PrintStringAtXY
-    ld hl, WarningPress, ld d, 12, ld e, 6, call UI_PrintStringAtXY
-    ld a, (1 << BUTTON_A_BIT), call WaitButton, call PlayBeepConfirm
-    pop hl, de, bc, af, ret
+    call UI_ClearScreen
+    ld a, 2
+    ld b, 4
+    ld c, 16
+    ld d, 10
+    call UI_DrawBox
+    ld hl, WarningTitle
+    ld c, 2
+    ld d, 4
+    ld e, 16
+    call UI_PrintInBox
+    ld hl, WarningMsg1
+    ld d, 7
+    ld e, 4
+    call UI_PrintStringAtXY
+    ld hl, WarningMsg2
+    ld d, 8
+    ld e, 4
+    call UI_PrintStringAtXY
+    ld hl, WarningMsg3
+    ld d, 9
+    ld e, 4
+    call UI_PrintStringAtXY
+    ld hl, WarningPress
+    ld d, 12
+    ld e, 6
+    call UI_PrintStringAtXY
+    ld a, (1 << BUTTON_A_BIT)
+    call WaitButton
+    call PlayBeepConfirm
+    pop hl, de, bc, af
+    ret
 
 DrawMenu:
     push af, bc, de, hl
-    call UI_ClearScreen, ld a, 1, ld b, 1, ld c, 18, ld d, 16, call UI_DrawBox
-    ld hl, MenuTitle, ld c, 1, ld d, 1, ld e, 18, call UI_PrintInBox
+    call UI_ClearScreen
+    ld a, 1
+    ld b, 1
+    ld c, 18
+    ld d, 16
+    call UI_DrawBox
+    ld hl, MenuTitle
+    ld c, 1
+    ld d, 1
+    ld e, 18
+    call UI_PrintInBox
     ld b, 0
 .draw_items_loop:
-    ld a, b, cp MENU_ITEMS, jr z, .draw_instr
-    push bc, push hl, ld a, b, add a, add 4, ld d, a
-    ld a, [CursorIndex], cp b, jr nz, .no_cursor
-    ld a, '>', ld e, 2, call UI_PrintAtXY
+    ld a, b
+    cp MENU_ITEMS
+    jr z, .draw_instr
+    push bc
+    push hl
+    ld a, b
+    add a
+    add 4
+    ld d, a
+    ld a, [CursorIndex]
+    cp b
+    jr nz, .no_cursor
+    ld a, '>'
+    ld e, 2
+    call UI_PrintAtXY
 .no_cursor:
-    ld hl, MenuPtrs, ld a, b, ld c, b, ld b, 0, add hl, bc, add hl, bc
-    ld e, [hl], inc hl, ld d, [hl], ex de, hl
-    ld d, [sp], ld e, 4, call UI_PrintStringAtXY
-    pop hl, pop bc, inc b, jr .draw_items_loop
+    ld hl, MenuPtrs
+    ld a, b
+    ld c, b
+    ld b, 0
+    add hl, bc
+    add hl, bc
+    ld e, [hl]
+    inc hl
+    ld d, [hl]
+    ex de, hl
+    ld d, [sp]
+    ld e, 4
+    call UI_PrintStringAtXY
+    pop hl
+    pop bc
+    inc b
+    jr .draw_items_loop
 .draw_instr:
-    ld hl, MenuInstr, ld d, 15, ld e, 2, call UI_PrintStringAtXY
-    pop hl, de, bc, af, ret
+    ld hl, MenuInstr
+    ld d, 15
+    ld e, 2
+    call UI_PrintStringAtXY
+    pop hl, de, bc, af
+    ret
 
 ; ====================================================================
 ; Variables Globales y Buffers Compartidos
--- src/sram.asm
+++ src/sram.asm
@@ -44,118 +44,312 @@ MsgCreated:          DB "Wallet creado.",0, MsgDeleted: DB "Wallet eliminado.",0
 SECTION "SramMenuCode", ROM1
 
 Entry_SRAM:
-    xor a, ld [sram_menu_cursor_pos], a
+    xor a
+    ld [sram_menu_cursor_pos], a
 .loop_menu:
-    call DrawSramMenu, call ReadJoypadWithDebounce
+    call DrawSramMenu
+    call ReadJoypadWithDebounce
     ld a, [JoyState]
-    bit BUTTON_UP_BIT, a,   jr nz, .move_up
-    bit BUTTON_DOWN_BIT, a, jr nz, .move_down
-    bit BUTTON_A_BIT, a,    jr nz, .select_option
-    bit BUTTON_B_BIT, a,    ret
+    bit BUTTON_UP_BIT, a
+    jr nz, .move_up
+    bit BUTTON_DOWN_BIT, a
+    jr nz, .move_down
+    bit BUTTON_A_BIT, a
+    jr nz, .select_option
+    bit BUTTON_B_BIT, a
+    ret
     jr .loop_menu
 
 .move_up:
-    ld a, [sram_menu_cursor_pos], or a, jr z, .wrap_bottom, dec a, jr .update_cursor
+    ld a, [sram_menu_cursor_pos]
+    or a
+    jr z, .wrap_bottom
+    dec a
+    jr .update_cursor
 .wrap_bottom: ld a, SRAM_MENU_ITEMS - 1
-.update_cursor: ld [sram_menu_cursor_pos], a, call PlayBeepNav, jr .loop_menu
+.update_cursor:
+    ld [sram_menu_cursor_pos], a
+    call PlayBeepNav
+    jr .loop_menu
 
 .move_down:
-    ld a, [sram_menu_cursor_pos], inc a, cp SRAM_MENU_ITEMS, jr c, .update_cursor, xor a, jr .update_cursor
+    ld a, [sram_menu_cursor_pos]
+    inc a
+    cp SRAM_MENU_ITEMS
+    jr c, .update_cursor
+    xor a
+    jr .update_cursor
 
 .select_option:
-    call PlayBeepConfirm, ld a, [sram_menu_cursor_pos]
-    cp 0, jp z, CreateWalletFlow
-    cp 1, jp z, SelectWalletFlow
-    cp 2, jp z, DeleteWalletFlow
+    call PlayBeepConfirm
+    ld a, [sram_menu_cursor_pos]
+    cp 0
+    jp z, CreateWalletFlow
+    cp 1
+    jp z, SelectWalletFlow
+    cp 2
+    jp z, DeleteWalletFlow
     ret
 
 ; ====================================================================
 ; Flujos de Lógica: Crear, Seleccionar, Borrar
 ; ====================================================================
 CreateWalletFlow:
-    call SRAM_GetWalletCount, cp MAX_WALLETS, jr nc, .limit_error
-    call GetText, CreateNamePrompt, NewWalletNameBuffer, WALLET_NAME_LEN
-    call GetText, CreateAddrPrompt, NewWalletAddrBuffer, WALLET_ADDR_LEN
-    ld hl, NewWalletNameBuffer, ld de, WALLET_NAME, ld bc, WALLET_NAME_LEN, call CopyString
-    ld hl, NewWalletAddrBuffer, ld de, WALLET_ADDR, ld bc, WALLET_ADDR_LEN, call CopyString
-    call SRAM_CreateWallet, call ShowMessage, MsgCreated, jp Entry_SRAM
-.limit_error: call ShowMessage, LimitErrorMsg, jp Entry_SRAM
+    call SRAM_GetWalletCount
+    cp MAX_WALLETS
+    jr nc, .limit_error
+    ld hl, CreateNamePrompt
+    ld de, NewWalletNameBuffer
+    ld c, WALLET_NAME_LEN
+    call GetText
+    ld hl, CreateAddrPrompt
+    ld de, NewWalletAddrBuffer
+    ld c, WALLET_ADDR_LEN
+    call GetText
+    ld hl, NewWalletNameBuffer
+    ld de, WALLET_NAME
+    ld bc, WALLET_NAME_LEN
+    call CopyString
+    ld hl, NewWalletAddrBuffer
+    ld de, WALLET_ADDR
+    ld bc, WALLET_ADDR_LEN
+    call CopyString
+    call SRAM_CreateWallet
+    ld hl, MsgCreated
+    call ShowMessage
+    jp Entry_SRAM
+.limit_error:
+    ld hl, LimitErrorMsg
+    call ShowMessage
+    jp Entry_SRAM
 
-SelectWalletFlow: ld a, LIST_MODE_SELECT, call WalletList_Show, jp Entry_SRAM
-DeleteWalletFlow: ld a, LIST_MODE_DELETE, call WalletList_Show, jp Entry_SRAM
+SelectWalletFlow:
+    ld a, LIST_MODE_SELECT
+    call WalletList_Show
+    jp Entry_SRAM
+DeleteWalletFlow:
+    ld a, LIST_MODE_DELETE
+    call WalletList_Show
+    jp Entry_SRAM
 
 WalletList_Show:
-    ld [sram_list_mode], a, call SRAM_GetWalletCount, or a, jr z, .no_wallets, ld [sram_wallet_count], a
-    ld b, 0, ld c, a, ld hl, sram_wallet_names_buffer
+    ld [sram_list_mode], a
+    call SRAM_GetWalletCount
+    or a
+    jr z, .no_wallets
+    ld [sram_wallet_count], a
+    ld b, 0
+    ld c, a
+    ld hl, sram_wallet_names_buffer
 .load_loop:
-    ld a, b, cp c, jr z, .loaded
-    push hl, push bc, ld a, b, call SRAM_LoadWallet, pop bc, pop hl, ld de, WALLET_NAME
-.copy_name: ld a, [de+], ld [hl+], a, or a, jr nz, .copy_name
-    inc b, jr .load_loop
+    ld a, b
+    cp c
+    jr z, .loaded
+    push hl
+    push bc
+    ld a, b
+    call SRAM_LoadWallet
+    pop bc
+    pop hl
+    ld de, WALLET_NAME
+.copy_name:
+    ld a, [de+]
+    ld [hl+], a
+    or a
+    jr nz, .copy_name
+    inc b
+    jr .load_loop
 .loaded:
-    xor a, ld [sram_list_cursor_pos], a
+    xor a
+    ld [sram_list_cursor_pos], a
 .list_loop:
-    call DrawWalletList, call ReadJoypadWithDebounce, ld a, [JoyState]
-    bit BUTTON_UP_BIT, a, jr nz, .list_up, bit BUTTON_DOWN_BIT, a, jr nz, .list_down
-    bit BUTTON_A_BIT, a, jr nz, .list_select, bit BUTTON_B_BIT, a, jr z, .list_loop, ret
+    call DrawWalletList
+    call ReadJoypadWithDebounce
+    ld a, [JoyState]
+    bit BUTTON_UP_BIT, a
+    jr nz, .list_up
+    bit BUTTON_DOWN_BIT, a
+    jr nz, .list_down
+    bit BUTTON_A_BIT, a
+    jr nz, .list_select
+    bit BUTTON_B_BIT, a
+    jr z, .list_loop
+    ret
 
-.no_wallets: call ShowMessage, NoWalletsMsg, ret
+.no_wallets:
+    ld hl, NoWalletsMsg
+    call ShowMessage
+    ret
 .list_up:
-    ld a, [sram_list_cursor_pos], or a, jr z, .list_wrap_bot, dec a, jr .list_upd
+    ld a, [sram_list_cursor_pos]
+    or a
+    jr z, .list_wrap_bot
+    dec a
+    jr .list_upd
 .list_wrap_bot: ld a, [sram_wallet_count], dec a
-.list_upd: ld [sram_list_cursor_pos], a, call PlayBeepNav, jr .list_loop
+.list_upd:
+    ld [sram_list_cursor_pos], a
+    call PlayBeepNav
+    jr .list_loop
 .list_down:
-    ld a, [sram_list_cursor_pos], inc a, ld b, a, ld a, [sram_wallet_count], cp b, jr c, .list_upd, xor a, jr .list_upd
+    ld a, [sram_list_cursor_pos]
+    inc a
+    ld b, a
+    ld a, [sram_wallet_count]
+    cp b
+    jr c, .list_upd
+    xor a
+    jr .list_upd
 .list_select:
-    call PlayBeepConfirm, ld a, [sram_list_mode], cp LIST_MODE_DELETE, jr z, .delete_wallet
-    ld a, [sram_list_cursor_pos], call SRAM_LoadWallet, call ShowMessage, MsgSelected, ret
+    call PlayBeepConfirm
+    ld a, [sram_list_mode]
+    cp LIST_MODE_DELETE
+    jr z, .delete_wallet
+    ld a, [sram_list_cursor_pos]
+    call SRAM_LoadWallet
+    ld hl, MsgSelected
+    call ShowMessage
+    ret
 .delete_wallet:
-    call ConfirmDelete, or a, jr z, .list_loop, ld a, [sram_list_cursor_pos], call SRAM_DeleteWallet, call ShowMessage, MsgDeleted, ret
+    call ConfirmDelete
+    or a
+    jr z, .list_loop
+    ld a, [sram_list_cursor_pos]
+    call SRAM_DeleteWallet
+    ld hl, MsgDeleted
+    call ShowMessage
+    ret
 
 ; ====================================================================
 ; Subrutinas y UI
 ; ====================================================================
 GetText: ; Entrada: HL=Prompt, DE=DestBuffer, C=MaxLen
-    push de, push bc
-    ld a, h, ld [InputPromptAddr+1], a, ld a, l, ld [InputPromptAddr], a
-    ld a, e, ld [InputDestBufAddr+1], a, ld a, d, ld [InputDestBufAddr], a
-    pop af, ld [InputMaxLen], a
-    call Entry_Input, pop de, pop bc, ret
+    push de
+    push bc
+    ld a, h
+    ld [InputPromptAddr+1], a
+    ld a, l
+    ld [InputPromptAddr], a
+    ld a, e
+    ld [InputDestBufAddr+1], a
+    ld a, d
+    ld [InputDestBufAddr], a
+    pop af
+    ld [InputMaxLen], a
+    call Entry_Input
+    pop de
+    pop bc
+    ret
 
 DrawSramMenu:
-    call UI_ClearScreen, ld hl, SramMenuTitle, ld d, 2, ld e, 2, call UI_PrintStringAtXY
-    ld hl, MenuOptionCreate, ld d, 5, ld e, 3, call DrawMenuItem, 0
-    ld hl, MenuOptionSelect, ld d, 6, ld e, 3, call DrawMenuItem, 1
-    ld hl, MenuOptionDelete, ld d, 7, ld e, 3, call DrawMenuItem, 2
-    ld hl, MenuOptionBack,   ld d, 8, ld e, 3, call DrawMenuItem, 3, ret
+    call UI_ClearScreen
+    ld hl, SramMenuTitle
+    ld d, 2
+    ld e, 2
+    call UI_PrintStringAtXY
+    ld hl, MenuOptionCreate
+    ld d, 5
+    ld e, 3
+    ld a, 0
+    call DrawMenuItem
+    ld hl, MenuOptionSelect
+    ld d, 6
+    ld e, 3
+    ld a, 1
+    call DrawMenuItem
+    ld hl, MenuOptionDelete
+    ld d, 7
+    ld e, 3
+    ld a, 2
+    call DrawMenuItem
+    ld hl, MenuOptionBack
+    ld d, 8
+    ld e, 3
+    ld a, 3
+    call DrawMenuItem
+    ret
 DrawMenuItem:
-    push af, ld a, [sram_menu_cursor_pos], cp [sp+2], jr nz, .no_cursor
-    ld a, '>', ld b, e, dec b, push de, ld e, b, call UI_PrintAtXY, pop de
-.no_cursor: call UI_PrintStringAtXY, pop af, ret
+    push af
+    ld a, [sram_menu_cursor_pos]
+    cp [sp+2]
+    jr nz, .no_cursor
+    ld a, '>'
+    ld b, e
+    dec b
+    push de
+    ld e, b
+    call UI_PrintAtXY
+    pop de
+.no_cursor:
+    call UI_PrintStringAtXY
+    pop af
+    ret
 
 DrawWalletList:
-    call UI_ClearScreen, ld a, [sram_list_mode], or a, jr z, .title_select
-    ld hl, ListTitleDelete, jr .draw_title
-.title_select: ld hl, ListTitleSelect
+    call UI_ClearScreen
+    ld a, [sram_list_mode]
+    or a
+    jr z, .title_select
+    ld hl, ListTitleDelete
+    jr .draw_title
+.title_select:
+    ld hl, ListTitleSelect
 .draw_title:
-    ld d, 2, ld e, 2, call UI_PrintStringAtXY
-    ld c, [sram_wallet_count], ld hl, sram_wallet_names_buffer, xor b
+    ld d, 2
+    ld e, 2
+    call UI_PrintStringAtXY
+    ld c, [sram_wallet_count]
+    ld hl, sram_wallet_names_buffer
+    xor b
 .loop_draw:
-    ld a, b, cp c, jr z, .done_draw
-    push hl, ld a, b, add 5, ld d, a
-    ld a, [sram_list_cursor_pos], cp b, jr nz, .no_cursor_list
-    ld a, '>', ld e, 1, call UI_PrintAtXY
+    ld a, b
+    cp c
+    jr z, .done_draw
+    push hl
+    ld a, b
+    add 5
+    ld d, a
+    ld a, [sram_list_cursor_pos]
+    cp b
+    jr nz, .no_cursor_list
+    ld a, '>'
+    ld e, 1
+    call UI_PrintAtXY
 .no_cursor_list:
-    ld e, 3, call UI_PrintStringAtXY, pop hl
-.skip_name: ld a, [hl+], or a, jr nz, .skip_name
-    inc b, jr .loop_draw
-.done_draw: ret
+    ld e, 3
+    call UI_PrintStringAtXY
+    pop hl
+.skip_name:
+    ld a, [hl+]
+    or a
+    jr nz, .skip_name
+    inc b
+    jr .loop_draw
+.done_draw:
+    ret
 
 ConfirmDelete:
-    call ShowMessageNoWait, ConfirmDeletePrompt
+    ld hl, ConfirmDeletePrompt
+    call ShowMessageNoWait
 .wait_confirm:
-    call ReadJoypad, ld a, [JoyState], ld c, a, and (1 << BUTTON_A_BIT) | (1 << BUTTON_B_BIT), cp c, jr z, .wait_confirm
-    ld a, [JoyState], bit BUTTON_A_BIT, a, ret
+    call ReadJoypad
+    ld a, [JoyState]
+    ld c, a
+    and (1 << BUTTON_A_BIT) | (1 << BUTTON_B_BIT)
+    cp c
+    jr z, .wait_confirm
+    ld a, [JoyState]
+    bit BUTTON_A_BIT, a
+    ret
 
-ShowMessage: call ShowMessageNoWait, ld a, (1 << BUTTON_A_BIT) | (1 << BUTTON_B_BIT), call WaitButton, ret
-ShowMessageNoWait: call UI_ClearScreen, ld d, 8, ld e, 3, call UI_PrintStringAtXY, ret
+ShowMessage:
+    call ShowMessageNoWait
+    ld a, (1 << BUTTON_A_BIT) | (1 << BUTTON_B_BIT)
+    call WaitButton
+    ret
+ShowMessageNoWait:
+    call UI_ClearScreen
+    ld d, 8
+    ld e, 3
+    call UI_PrintStringAtXY
+    ret
